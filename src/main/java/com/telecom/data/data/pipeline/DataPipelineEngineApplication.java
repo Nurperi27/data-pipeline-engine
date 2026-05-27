0421598f0dd456b86e96e2e54193fdb66c759b6b
n <nurperies003@gmail.com>
2026-05-27 15:09:38 +0600

    Initial commit

---
+++ src/main/java/com/telecom/data/data/pipeline/DataPipelineEngineApplication.java
@@ -0,0 +1,13 @@
+package com.telecom.data.data.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPipelineEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPipelineEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telecom/data/data/pipeline/DataPipelineEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.data.data.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPipelineEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
